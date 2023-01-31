9a5c1e20d6f3dea6fb0490804d2bd57f4891ca35
Bianca-karoline <113278939+Bianca-karoline@users.noreply.github.com>
2023-01-31 17:13:50 -0300

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class HelloWorld {
-    public static void main (String[] args){
-        System.out.println("Hello World");
-        String[] paises = {"Brasil", "Russia"};
-        System.out.println(Arrays.toString(paises));
-    }
-}
